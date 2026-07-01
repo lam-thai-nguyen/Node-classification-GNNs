d926e8f2ef43354963d4c1999813325661fcd9eb
thanhvv2605 <thckpck11@gmail.com>
2026-07-01 10:17:00 +0700

    relu -> elu

--- models/gat.py
+++ models/gat.py
@@ -14,14 +14,14 @@ class GAT(nn.Module):
             self.conv_layers.append(GATConv(hidden_channels*heads, hidden_channels, heads, concat=True, dropout=dropout))
             self.bn_layers.append(nn.BatchNorm1d(hidden_channels*heads))
         self.classifier = GATConv(hidden_channels*heads, out_channels, heads=1, concat=False, dropout=dropout)
-        self.relu = nn.ReLU(inplace=True)
+        self.elu = nn.ELU(inplace=True)
         self.dropout = nn.Dropout(p=dropout)
     
     def forward(self, x, edge_index):
         for conv, bn in zip(self.conv_layers, self.bn_layers):
             x = conv(x, edge_index)
             x = bn(x)
-            x = self.relu(x)
+            x = self.elu(x)
             x = self.dropout(x)
         logits = self.classifier(x, edge_index)
         return logits

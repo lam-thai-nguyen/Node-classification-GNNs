ebb6617e6b701836ef20df92437f96875eef2b67
thanhvv2605 <thckpck11@gmail.com>
2026-07-07 11:20:00 +0700

    modify: remove argument `heads` and fix it to 1 for simplicity

--- models/gat.py
+++ models/gat.py
@@ -3,17 +3,17 @@ from torch_geometric.nn import GATConv
 
 
 class GAT(nn.Module):
-    def __init__(self, in_channels=128, out_channels=40, hidden_channels=128, heads=1, num_layers=3, dropout=0.5):
+    def __init__(self, in_channels=128, out_channels=40, hidden_channels=128, num_layers=3, dropout=0.5):
         super().__init__()
 
         self.conv_layers = nn.ModuleList()
         self.bn_layers = nn.ModuleList()
-        self.conv_layers.append(GATConv(in_channels, hidden_channels, heads, concat=True, dropout=dropout))
-        self.bn_layers.append(nn.BatchNorm1d(hidden_channels*heads))
+        self.conv_layers.append(GATConv(in_channels, hidden_channels, heads=1, concat=True, dropout=dropout))
+        self.bn_layers.append(nn.BatchNorm1d(hidden_channels))
         for _ in range(num_layers-2):
-            self.conv_layers.append(GATConv(hidden_channels*heads, hidden_channels, heads, concat=True, dropout=dropout))
-            self.bn_layers.append(nn.BatchNorm1d(hidden_channels*heads))
-        self.classifier = GATConv(hidden_channels*heads, out_channels, heads=1, concat=False, dropout=dropout)
+            self.conv_layers.append(GATConv(hidden_channels, hidden_channels, heads=1, concat=True, dropout=dropout))
+            self.bn_layers.append(nn.BatchNorm1d(hidden_channels))
+        self.classifier = GATConv(hidden_channels, out_channels, heads=1, concat=False, dropout=dropout)
         self.elu = nn.ELU(inplace=True)
         self.dropout = nn.Dropout(p=dropout)
     
@@ -25,3 +25,4 @@ class GAT(nn.Module):
             x = self.dropout(x)
         logits = self.classifier(x, edge_index)
         return logits
+    
